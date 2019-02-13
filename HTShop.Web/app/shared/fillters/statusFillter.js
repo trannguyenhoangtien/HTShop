70c0be656ef009f120a415c974d1cb4f8624b731
hoangtien2510 <hoangtien103@gmail.com>
2019-02-13 21:44:38 +0700

    add filter

---
+++ HTShop.Web/app/shared/fillters/statusFillter.js
@@ -0,0 +1,9 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true)
+                return 'Kích hoạt';
+            return 'Khóa';
+        }
+    });
+})(angular.module('htshop.common'));
